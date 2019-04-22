242e33ea514e7c732bfcf97add6adeb249d7a17f
Carlos Lantigua <clantigua954@gmail.com>
2019-04-21 21:42:02 -0400

    setup axios instances

--- recordstore-front/src/main.js
+++ recordstore-front/src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
+import './main.css'
 
 Vue.config.productionTip = false
 
