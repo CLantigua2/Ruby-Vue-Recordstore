d985487cb4721222b05c467b13ddb816e1c559c9
Carlos Lantigua <clantigua954@gmail.com>
2019-04-23 23:07:04 -0400

    built out the base for the records component

---
+++ recordstore-front/src/components/records/Records.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="max-w-md m-auto py-10">
+        <div class="text-red" v-if="error">{{ error }} </div>
+        <h3 class="font-mono font-regular text-3xl mb-4">Add a new record</h3>
+
+        <form @submit.prevent="addRecord">
+            <div class="mb-6">
+                <label class="label" for="recort_title">Title</label>
+                <input
+                type="text"
+                id="record_title"
+                class="input"
+                autofocus
+                placeholder="Type a record name"
+                v-model="newRecord.title"
+                >
+            </div>
+
+            <div class="mb-6">
+                <label class="label" for="recort_year">Year</label>
+                <input
+                type="text"
+                id="record_year"
+                class="input"
+                autofocus
+                placeholder="Year"
+                v-model="newRecord.year"
+                >
+            </div>
+
+            <div class="mb-6">
+                <label class="label" for="artist">Artist</label>
+                <select id="artist" class="select" v-model="newRecord.artist">
+                    <option disabled value="">Select an artist</option>
+                    <option :value="artist.id" v-for="artist in artists" :key="artist.id">{{ artist.name }}</option>
+                </select>
+                    <p class="pt-4">Don't see an artist? <router-link to="/artist" class="link-grey">Create One</router-link></p>
+            </div>
+
+            <input type="submit" value="Add Record" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green hover:bg-green-dark block w-full py-4 text-white items-center justify-center">
+        </form>
+
+        <hr class="border border-grey-light my-6" />
+
+        <ul class="list-reset mt-4">
+            <li class="py-4" v-for="record in records" :key="record.id" :record="record">
+                <div class="flex items-center justify-between flex-wrap">
+                    <p class="block flex font-mono font-semibod items-center">
+                        <svg class="fill-current text-indigo w-6 h-6 mr-2" viewBox="0 0 24 24" width="24" height="24"><title>record vinyl</title><path d="M23.938 10.773a11.915 11.915 0 0 0-2.333-5.944 12.118 12.118 0 0 0-1.12-1.314A11.962 11.62 0 0 0 12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12c0-.414-.021-.823-.062-1.227zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 08zm0-5a1 1 0 1 0 0 2 1 1 0 0 0 0-2z">{{ record.title }} &mdash; {{record.year }}</path></svg>
+                    </p>
+                    <p class="block font-mono font-semibold">{{ getArtist(record) }}</p>
+                </div>
+
+                <button class="bg-transparent text-sm hover:bg-blue hover:text-white text-blue border border-blue no-underline font-bold py-2 px-4 mr-2 rounded" @click.prevent="editRecord(record)">Edit</button>
+
+                <button class="bg-transparent text-sm hover:bg-red text-red hover:text-white no-underline font-bold py-2 px-4 rounded border border-red" @click.prevent="removeRecord(record)">Delete</button>
+            </li>
+        </ul>
+
+    </div>
+</template>
